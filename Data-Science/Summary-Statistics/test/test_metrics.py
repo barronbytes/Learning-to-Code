45d22e6a7b5b39936d59949953a260255b318f7d
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 10:06:13 -0400

    Created setUp() resource method for data

--- Data-Science/Summary-Statistics/test/test_metrics.py
+++ Data-Science/Summary-Statistics/test/test_metrics.py
@@ -4,12 +4,16 @@ from metrics import Metrics
 
 
 class TestMetrics(unittest.TestCase):
-    
-    def test_average(self):
-        data = [1, 2, 3, 4, 5, 6, 7]
-        avg = stats.mean(data)
-        self.assertEqual(Metrics().average(data), round(avg, 2))
 
+    def setUp(self):
+        self.data = [1, 2, 3, 4, 5, 6, 7]
+
+    def test_average(self):
+        avg = stats.mean(self.data)
+        self.assertEqual(Metrics().average(self.data), round(avg, 2))
+
+    def test_maximum(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
