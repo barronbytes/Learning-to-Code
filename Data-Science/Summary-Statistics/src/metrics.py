f8b751fe2b00114db36fdc7161de588150777db4
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 11:37:09 -0400

    metrics: included method docstrings

--- Data-Science/Summary-Statistics/src/metrics.py
+++ Data-Science/Summary-Statistics/src/metrics.py
@@ -2,6 +2,15 @@ class Metrics():
     
     @staticmethod
     def average(data: list) -> float:
+        '''
+        Calculates average for list of heart rate population numbers.
+        Treat list as poplulation numbers.
+
+        Args:
+            data (list[int]): Heart rate population numbers.
+        Returns:
+            float: Average heart rate from heart rate data.
+        '''
         sum = 0
         for num in data:
             sum += num
@@ -9,6 +18,14 @@ class Metrics():
 
     @staticmethod
     def maximum(data: list) -> float:
+        '''
+        Finds maximum heart rate from heart rate values.
+
+        Args:
+            data (list[int]): Heart rate population numbers.
+        Returns:
+            float: Maximum heart rate from heart rate data.
+        '''
         max = float("-inf")
         for num in data:
             if num > max:
@@ -17,6 +34,14 @@ class Metrics():
 
     @staticmethod
     def variance(data: list) -> float:
+        '''
+        Calculates population variance from heart rate values.
+
+        Args:
+            data (list[int]): Heart rate population numbers.
+        Returns:
+            float: Population variance from heart rate data.
+        '''
         mean = Metrics.average(data)
         numerator = 0
         for num in data:
@@ -25,5 +50,13 @@ class Metrics():
 
     @staticmethod
     def standard_deviation(data: list) -> float:
+        '''
+        Calculates population standard deviation from heart rate values.
+
+        Args:
+            data (list[int]): Heart rate population numbers.
+        Returns:
+            float: Population standard deviation heart rate data.
+        '''
         sigma = Metrics.variance(data)
         return round(sigma**0.5, 2)
