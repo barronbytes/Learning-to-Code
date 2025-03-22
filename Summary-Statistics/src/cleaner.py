5542bd434bdaca879a0bcc0496b3748ed6b9c745
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 00:07:33 -0400

    cleaner: passed all tests

--- Summary-Statistics/src/cleaner.py
+++ Summary-Statistics/src/cleaner.py
@@ -23,12 +23,18 @@ class Cleaner():
     def is_list(self) -> bool:
         return isinstance(self.data, list)
     
-    def clean_data(data: list) -> list | None:
+    def clean_data(data: list) -> list[int] | None:
         data_strings = [d.strip() for d in data if isinstance(d, str)]
         data_numbers = [int(d) for d in data_strings if d.isdigit()]
         return data_numbers if data_numbers else None
 
     def brain(self) -> list[int] | None:
+        '''
+        For list input, filters string values, removes whitespace, and type casts values to integers.
+
+        Returns:
+            list[int] | None: For valid input, returns a list of integers; otherwise, returns None.
+        '''
         is_list = self.is_list()
         clean_data = Cleaner.clean_data(self.data) if is_list else None
         return clean_data
--- Summary-Statistics/test/test_cleaner.py
+++ Summary-Statistics/test/test_cleaner.py
@@ -19,5 +19,12 @@ class TestCleaner(unittest.TestCase):
         data = ["  Hello  ", "  World  \n"]
         self.assertEqual(Cleaner(data).brain(), None)
 
+    def test_data_list_numbers_1(self):
+        data_1 = ["1\n", "\n", "3\n", "4\n", "5\n", "\n", "7\n"]
+        data_2 = ["1\n", "NO DATA\n", "  3  \n", "4\n", "5\n", "NO DATA\n", "7\n"]
+        data_expected = [1, 3, 4, 5, 7]
+        self.assertListEqual(Cleaner(data_1).brain(), data_expected)
+        self.assertListEqual(Cleaner(data_2).brain(), data_expected)
+
 if __name__ == "__main__":
     unittest.main()
