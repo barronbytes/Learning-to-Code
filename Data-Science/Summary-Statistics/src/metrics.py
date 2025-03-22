1771b07ac0339f2541db55181a58f152561a679c
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 09:48:12 -0400

    metrics: average() method passed tests

--- Data-Science/Summary-Statistics/src/metrics.py
+++ Data-Science/Summary-Statistics/src/metrics.py
@@ -1,13 +1,24 @@
 class Metrics():
     
-    def average():
-        pass
+    @staticmethod
+    def average(data: list) -> float:
+        sum = 0
+        for num in data:
+            sum += num
+        return round(sum/len(data), 2)
 
-    def maximum():
-        pass
+    @staticmethod
+    def maximum(data: list) -> float:
+        max = float("-inf")
+        for num in data:
+            if num > max:
+                max = num
+        return max
 
+    @staticmethod
     def variance():
         pass
 
+    @staticmethod
     def standard_deviation():
         pass
--- Data-Science/Summary-Statistics/test/test_metrics.py
+++ Data-Science/Summary-Statistics/test/test_metrics.py
@@ -1,9 +1,14 @@
 import unittest
+import statistics as stats
 from metrics import Metrics
 
 
 class TestMetrics(unittest.TestCase):
-    pass
+    
+    def test_average(self):
+        data = [1, 2, 3, 4, 5, 6, 7]
+        avg = stats.mean(data)
+        self.assertEqual(Metrics().average(data), round(avg, 2))
 
 
 if __name__ == "__main__":
