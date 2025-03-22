947f46157753eca1de64a009646c387ec88acb4f
barronbytes <jonathan.m.barron@gmail.com>
2025-03-21 23:45:14 -0400

    cleaner: passed tests for empty lists and list of letters

--- Summary-Statistics/src/cleaner.py
+++ Summary-Statistics/src/cleaner.py
@@ -19,3 +19,16 @@ class Cleaner():
         if isinstance(other, Cleaner):
             equality = self.data == other.data
         return equality
+    
+    def is_list(self) -> bool:
+        return isinstance(self.data, list)
+    
+    def clean_data(data: list) -> list | None:
+        data_strings = [d.strip() for d in data if isinstance(d, str)]
+        data_numbers = [int(d) for d in data_strings if d.isdigit()]
+        return data_numbers if data_numbers else None
+
+    def brain(self) -> list[int] | None:
+        is_list = self.is_list()
+        clean_data = Cleaner.clean_data(self.data) if is_list else None
+        return clean_data
--- Summary-Statistics/test/test_cleaner.py
+++ Summary-Statistics/test/test_cleaner.py
@@ -3,5 +3,21 @@ from cleaner import Cleaner
 
 class TestCleaner(unittest.TestCase):
 
-    def test_is_string_list(self):
-        self.assertEqual("2", "1")
+    def test_is_list(self):
+        data = []
+        self.assertTrue(Cleaner(data).is_list())
+
+    def test_data_not_list(self):
+        data = "Hello World"
+        self.assertEqual(Cleaner(data).brain(), None)
+
+    def test_data_list_empty(self):
+        data = []
+        self.assertEqual(Cleaner(data).brain(), None)
+
+    def test_data_list_letters(self):
+        data = ["  Hello  ", "  World  \n"]
+        self.assertEqual(Cleaner(data).brain(), None)
+
+if __name__ == "__main__":
+    unittest.main()
