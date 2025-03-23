285c1bf56e69a1af43374fa372c49e011773daa0
barronbytes <jonathan.m.barron@gmail.com>
2025-03-23 09:31:09 -0400

    visualize: logic for histogram method completed

--- Data-Science/Summary-Statistics/src/visualize.py
+++ Data-Science/Summary-Statistics/src/visualize.py
@@ -8,7 +8,7 @@ class Visualize():
     image_dir_name = "images"
 
     @staticmethod
-    def line_plot(file: str, data: list[int]) -> None:
+    def line_plot(file: str, data: list[int], images_dir: str) -> None:
         # set data
         time_increment = 5
         time = time_increment*len(data)
@@ -23,16 +23,13 @@ class Visualize():
         axes.set(xlabel="Time (minutes)", ylabel="Heart Rate (bpm)", title=f"Heart Rate Data for {file}")
         axes.grid()
 
-        # directory for saving data
-        images_dir = os.path.join(os.path.dirname(__file__), "..", Visualize.image_dir_name)
-        os.makedirs(images_dir, exist_ok=True)
-
         # save data, no need for plt.show() in this project
-        figure.savefig(os.path.join(images_dir, "line_plot.png"))
+        file_name = file.replace(".txt", "")
+        figure.savefig(os.path.join(images_dir, f"{file_name}_line_plot.png"))
 
 
     @staticmethod
-    def box_plot(file: str, data: list[int]) -> None:
+    def box_plot(file: str, data: list[int], images_dir: str) -> None:
         # plot data
         figure, axes = plt.subplots()
         axes.boxplot(data, vert=False)  # Set vertical=False for horizontal box plot
@@ -41,9 +38,35 @@ class Visualize():
         axes.set(xlabel="Heart Rate (bpm)", ylabel="Study Participant", title=f"Heart Rate Box Plot for {file}")
         axes.grid()
 
+        # save data, no need for plt.show() in this project
+        file_name = file.replace(".txt", "")
+        figure.savefig(os.path.join(images_dir, f"{file_name}_box_plot.png"))
+
+
+    @staticmethod
+    def histogram(file: str, data: list[int], images_dir: str) -> None:
+        # set data
+        bins = 10
+
+        # plot data
+        figure, axes = plt.subplots()
+        axes.hist(data, bins=bins, edgecolor="black")
+
+        # label data
+        axes.set(xlabel="Heart Rate (bpm)", ylabel="Frequency", title=f"Heart Rate Histogram for {file}")
+        axes.grid()
+
+        # save data, no need for plt.show() in this project
+        file_name = file.replace(".txt", "")
+        figure.savefig(os.path.join(images_dir, f"{file_name}_histogram.png"))
+
+
+    @staticmethod
+    def brain(file: str, data: list[int]) -> None:
         # directory for saving data
         images_dir = os.path.join(os.path.dirname(__file__), "..", Visualize.image_dir_name)
         os.makedirs(images_dir, exist_ok=True)
 
-        # save data, no need for plt.show() in this project
-        figure.savefig(os.path.join(images_dir, "box_plot.png"))
+        Visualize.box_plot(file, data, images_dir)
+        Visualize.line_plot(file, data, images_dir)
+        Visualize.histogram(file, data, images_dir)
