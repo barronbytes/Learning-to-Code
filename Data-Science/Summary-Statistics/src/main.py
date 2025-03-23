0b7d62470a43c73a7a4f59abbb127e226ea9021f
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 20:02:10 -0400

    main: display_results() method created

--- Data-Science/Summary-Statistics/src/main.py
+++ Data-Science/Summary-Statistics/src/main.py
@@ -2,11 +2,16 @@ from get_file_data import GetFileData
 from cleaner import Cleaner
 from metrics import Metrics
 
+@staticmethod
+def display_results(stats: list[float]) -> str:
+    max_num, avg, stdev = stats
+    print(f"Maximum HR: {max_num} \nAverage HR: {avg} \nStandard Deviation HR: {stdev}")
+
 def main():
     raw_data = GetFileData.brain()
     clean_data = Cleaner(raw_data).brain()
     stats = Metrics().brain(clean_data) if isinstance(clean_data, list) and len(clean_data) >= 1 else [0, 0, 0]
-    print(stats)
+    display_results(stats)
 
 # run main() only when executed directly by main.py, not when imported by other files
 if __name__ == "__main__":
