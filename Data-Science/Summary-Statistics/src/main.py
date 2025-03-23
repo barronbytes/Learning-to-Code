dbf3cc38acb0ea1bd0a1b0974922dda01e6d1057
barronbytes <jonathan.m.barron@gmail.com>
2025-03-22 23:52:24 -0400

    main: implemented Visualize class method of line_plot

--- Data-Science/Summary-Statistics/src/main.py
+++ Data-Science/Summary-Statistics/src/main.py
@@ -1,6 +1,8 @@
 from get_file_data import GetFileData
 from cleaner import Cleaner
 from metrics import Metrics
+from visualize import Visualize
+
 
 @staticmethod
 def display_results(stats: list[float]) -> str:
@@ -11,7 +13,8 @@ def main():
     raw_data = GetFileData.brain()
     clean_data = Cleaner(raw_data).brain()
     stats = Metrics().brain(clean_data) if isinstance(clean_data, list) and len(clean_data) >= 1 else [0, 0, 0]
-    display_results(stats)
+    Visualize.line_plot(data=clean_data, time_increment=5)
+
 
 # run main() only when executed directly by main.py, not when imported by other files
 if __name__ == "__main__":
