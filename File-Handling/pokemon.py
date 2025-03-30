f7592255374aa80d156379c2a419e5ea6f4383e9
barronbytes <jonathan.m.barron@gmail.com>
2025-03-30 00:11:52 -0400

    file-handling: update_data() and update_data() logic completed

--- File-Handling/pokemon.py
+++ File-Handling/pokemon.py
@@ -1,23 +1,40 @@
 import json
+import copy
 
 
 file_path = "pokemon_types.json"
-new_file_path = "updated_pokemon_types.json"
 
 
-# reading (deserializing) data
-def read_file(file_path: str) -> dict:
+# serialize data (create file)
+def create_data(data: dict, file_path: str) -> None:
+    with open(file_path, mode="w") as new_file:
+        json.dump(data, new_file, indent=4)
+
+
+# deserialize data (read file)
+def read_data(file_path: str) -> dict:
     with open(file_path, mode="r") as file:
         data = json.load(file)
     return data
 
 
-# serializing data
-def create_file(data: dict, new_file_path: str) -> None:
-    with open(new_file_path, mode="w") as new_file:
-        json.dump(data, new_file, indent=4)
+# update data
+def update_data(data: str) -> dict:
+    new_key = "psychic"
+    new_value = {
+        "name": "Psychic",
+        "weaknesses": ["Bug", "Ghost", "Dark"],
+        "strengths": ["Fighting", "Poison"],
+        "immunities": [],
+    }
+    data["types"][new_key] = new_value
+    return data
 
 
-pokemon_data = read_file(file_path)
-pokemon_types = pokemon_data["types"].keys()
+raw_data = read_data(file_path)
+updated_data = update_data(copy.deepcopy(raw_data))
+create_data(updated_data, file_path)
+
+pokemon_types = raw_data["types"].keys()
 print("\n".join(f"Type: {d}" for d in pokemon_types))
+print(raw_data == updated_data)
