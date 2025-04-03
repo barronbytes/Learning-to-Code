c7b41e348a719e5eab06869c8ba78bf83ce12ac8
barronbytes <jonathan.m.barron@gmail.com>
2025-04-02 23:06:18 -0400

    config: endpoint variable name change

--- APIs/GoREST/config.py
+++ APIs/GoREST/config.py
@@ -4,7 +4,7 @@ import os
 BASE_URL = "https://gorest.co.in/"
 BASE_PATH = "public/"
 VERSION = "v2/"
-USERS = "users/"
+ENDPOINT = "users/"
 
 
 def get_access_token():
@@ -12,4 +12,4 @@ def get_access_token():
 
 
 def get_request_url():
-    return BASE_URL + BASE_PATH + VERSION + USERS
+    return BASE_URL + BASE_PATH + VERSION + ENDPOINT
