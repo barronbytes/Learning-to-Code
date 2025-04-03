1236e84b922afec540c8c2a24ce48a2e4e2882f2
barronbytes <jonathan.m.barron@gmail.com>
2025-04-03 00:08:30 -0400

    get: timeout loop completed with try-catch block

--- APIs/GoREST/get.py
+++ APIs/GoREST/get.py
@@ -1,6 +1,23 @@
 import requests
 import config
+import time
 
 
-r = requests.get(config.get_request_url())
-print(r.status_code)
+MAX_ATTEMPTS = 3
+TIMEOUT = 1.2
+request_url = config.get_request_url()
+
+
+for i in range(1, MAX_ATTEMPTS+1):
+    try:
+        response = requests.get(request_url, timeout=TIMEOUT)
+        data = response.json()
+        break
+    except (requests.exceptions.HTTPError, 
+        requests.exceptions.Timeout, 
+        requests.exceptions.RequestException, 
+        ValueError) as e:
+        print(f"Attempt {i} failed: {e}")
+
+
+print(f"{data}")
