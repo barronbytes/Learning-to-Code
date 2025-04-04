ee96ee553ff644e5791c1418d4a48c59e8287113
barronbytes <jonathan.m.barron@gmail.com>
2025-04-03 21:43:51 -0400

    get file modified to query user id

--- APIs/GoREST/get.py
+++ APIs/GoREST/get.py
@@ -4,7 +4,8 @@ import config
 
 MAX_ATTEMPTS = 3
 TIMEOUT = 1.2
-request_url = config.get_request_url()
+user_id= "7810784"
+request_url = config.get_request_url()+user_id
 
 
 for i in range(1, MAX_ATTEMPTS+1):
