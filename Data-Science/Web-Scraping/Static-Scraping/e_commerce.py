66d49f1b2e2b320d4c6ef7a70e5f3166c5e9b61a
barronbytes <jonathan.m.barron@gmail.com>
2025-04-09 00:40:30 -0400

    pagination: scrape_prices() works

--- Data-Science/Web-Scraping/Static-Scraping/e_commerce.py
+++ Data-Science/Web-Scraping/Static-Scraping/e_commerce.py
@@ -3,8 +3,11 @@ from bs4 import BeautifulSoup as bs
 from pagination import Pagination
 
 
-BASE_URL = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page=1"
+BASE_URL = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops"
 r = requests.get(BASE_URL)
 soup = bs(r.content, "lxml")
 page_count = Pagination().calc_page_count(soup)
-print(page_count)
+products = Pagination().scrape_products(soup)
+prices = Pagination.scrape_prices(soup)
+print(products)
+print(prices)
--- Data-Science/Web-Scraping/Static-Scraping/pagination.py
+++ Data-Science/Web-Scraping/Static-Scraping/pagination.py
@@ -1,10 +1,25 @@
 from bs4 import BeautifulSoup as bs
+from bs4.element import Tag
 
 
 class Pagination():
 
+
     @staticmethod
     def calc_page_count(soup: bs) -> int:
-        parent_tag = soup.find("ul", attrs={"class":"pagination"})
+        parent_tag = soup.select_one("ul.pagination")
         anchors = parent_tag.select("a.page-link")
         return int(anchors[-2].text)
+
+
+    @staticmethod
+    def scrape_products(soup: bs) -> list[str]:
+        products = soup.select("a.title")
+        return [p.text.strip() for p in products]
+
+
+    @staticmethod
+    def scrape_prices(soup: bs) -> list[str]:
+        parent_tags = soup.select("div.caption")
+        price_tags = [pt.select_one("span[itemprop='price']") for pt in parent_tags] # cannot use span.price !!!
+        return [pt.text.strip() for pt in price_tags]
