beda0ec9f5e05c71f7637554dc47df804866894a
barronbytes <jonathan.m.barron@gmail.com>
2025-04-10 21:57:48 -0400

    Extraction: file completed but missing annotations

--- APIs/OpenAI/src/control.py
+++ APIs/OpenAI/src/control.py
@@ -4,11 +4,11 @@ import os
 class Control():
     @staticmethod
     def clear_screen() -> bool:
-        """
+        '''
         Decides whether to continue or exit program.
         Returns:
             bool: Updated value for `run_all` variable from main.py
-        """
+        '''
         ask_user = input("Run program again?: (Yes/No) ")
         os.system('cls' if os.name == 'nt' else 'clear')
         return ask_user.lower() == "yes"
