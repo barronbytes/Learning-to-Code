3e756861d9c3be3cd5c1f929814687a7bf4bdc03
barronbytes <jonathan.m.barron@gmail.com>
2025-04-17 23:26:14 -0400

    main: else corrected from returining [] > ([], {})

--- APIs/OpenAI/src/main.py
+++ APIs/OpenAI/src/main.py
@@ -8,7 +8,7 @@ def main():
     run_all = True
     while run_all:
         is_extracted, file_name, reviews = Extract.brain()
-        data_labels, sentiments = Transform.brain(reviews) if is_extracted else []
+        data_labels, sentiments = Transform.brain(reviews) if is_extracted else ([], {})
         data_counts = [sentiments[label] for label in data_labels]
         Load.create_bar_graph(file_name, data_labels, data_counts)
         run_all = Control.clear_screen() if is_extracted else False
