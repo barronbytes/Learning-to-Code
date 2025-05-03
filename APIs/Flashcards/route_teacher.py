226ee4e253de4fd9f743361ff2a237d7d2b4a765
barronbytes <jonathan.m.barron@gmail.com>
2025-05-02 23:58:54 -0400

    CRUD: the READ teacher user route completed

--- APIs/Flashcards/route_teacher.py
+++ APIs/Flashcards/route_teacher.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter
 from schema_deck import Deck
-from service_deck import create_deck
+from service_deck import create_deck, read_deck
 
 
 router = APIRouter(prefix="/teacher", tags=["Teacher"])
 
 
 # use plural nouns for resources
-@router.post(path="/deck", response_model=Deck)
+@router.post(path="/decks", response_model=Deck)
 def create_deck_route(name: str) -> Deck:
     return create_deck(name)
+
+
+@router.get(path="/decks/{name}", response_model=Deck)
+def read_deck_route(name: str) -> Deck:
+    return read_deck(name)
--- APIs/Flashcards/service_deck.py
+++ APIs/Flashcards/service_deck.py
@@ -1,12 +1,13 @@
 from schema_deck import Deck
-from model_database import db
+import model_database # ensure shared db is modified
 
 
 def create_deck(name: str) -> Deck:
     deck = Deck(deck_id=name, cards=[])
-    db[name] = deck
+    model_database.db[name] = deck
     return deck
 
 
 def read_deck(name: str) -> Deck:
-    pass
+    deck = model_database.db.get(name)
+    return deck
