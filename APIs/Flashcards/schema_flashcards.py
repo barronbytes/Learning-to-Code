fb3f6770ee1f2aecd1ed3acba0686215394056dc
barronbytes <jonathan.m.barron@gmail.com>
2025-07-21 23:39:39 -0400

    Deleted API subfolder

--- APIs/Flashcards/schema_flashcards.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from schema_deck import Deck
-
-
-class Flashcards(BaseModel):
-    flashcards: dict[str, Deck]
--- APIs/OpenAI/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# set PYTHONPATH folder, search appropriate directory for test files
-PYTHONPATH=src python3 -m unittest discover -s test
